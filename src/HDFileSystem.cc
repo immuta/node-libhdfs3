9a7821f3448cba6745ea02a8bff1f164ebfd8d48
Barrett <barrett@immuta.com>
2018-07-19 10:46:10 -0400

    debug

--- src/HDFileSystem.cc
+++ src/HDFileSystem.cc
@@ -73,24 +73,23 @@ void HDFileSystem::UV_Connect(uv_work_t* req) {
 
     connect_work_data* data = (connect_work_data *)(req->data);
     DEBUG("HDFileSystem::UV_Connect2");
+    char* errorMessage = NULL;
     try {
         DEBUG("HDFileSystem::UV_Connect3");
         data->fileSystem->fs = hdfsBuilderConnect(data->bld);
         DEBUG("HDFileSystem::UV_Connect4");
-    } catch (const Hdfs::HdfsException & e) {
+    } catch (Hdfs::HdfsException &e) {
         DEBUG("HDFileSystem::UV_Connect-stuff went down0");
-        const char* errorMessage = e.what();
+        errorMessage = e.what();
         DEBUG("HDFileSystem::UV_Connect-stuff went down1");
         DEBUG(errorMessage);
     
     } catch (...) {
-    //} catch (const std::exception& e) {
-        //errorMessage = e.what();
-        DEBUG("HDFileSystem::UV_Connect-stuff went down2");
+        // Keep any other exceptions from falling through. hdfsGetLastError() below will grab these.
     }
     if (!data->fileSystem->fs) {
         data->error = errno;
-        data->errMsg = hdfsGetLastError();
+        data->errMsg = errorMessage != NULL ? errorMessage : hdfsGetLastError();
     }
 }
 
